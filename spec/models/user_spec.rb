421a2a12481e63670a35ed26f8bdf895fc27b393
Simon Garcia <simon.garciar@gmail.com>
2014-02-11 15:50:27 +0100

    install devise and run generator for user model, set some pending tests for the User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe User do
+  context "when authenticating" do
+    it "should have an email" do
+      pending
+    end
+    it "should have a password" do
+      pending
+    end
+  end
+
+
+  context "when registering" do
+    it "should not accept invalid emails" do
+      pending
+    end
+  end
+end
